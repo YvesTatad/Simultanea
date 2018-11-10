2887f60e291e1382ed40f607bc980d08c7b9f06a
CJ <christianjerom29@gmail.com>
2018-11-10 11:57:11 +0800

    created MobSelectionActivity

--- app/src/main/java/com/sombright/vizix/simultanea/MobCharacters/MobPool.java
+++ app/src/main/java/com/sombright/vizix/simultanea/MobCharacters/MobPool.java
@@ -1,8 +1,13 @@
 package com.sombright.vizix.simultanea.MobCharacters;
 
+import android.database.DataSetObserver;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+
 import com.sombright.vizix.simultanea.R;
 
-class MobPool {
+class MobPool implements ListAdapter {
     static final com.sombright.vizix.simultanea.MobCharacters.MobModel[] mobList = {
             new com.sombright.vizix.simultanea.MobCharacters.MobModel(
                     R.string.skeleton_name,
@@ -32,5 +37,65 @@ class MobPool {
     private static final int DEFAULT_MOB_INDEX = 0;
     static com.sombright.vizix.simultanea.MobCharacters.MobModel getDefaultMob() {
         return mobList[DEFAULT_MOB_INDEX];
+    }
+
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return false;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
     }
 }
--- app/src/main/java/com/sombright/vizix/simultanea/MobCharacters/MobSelectionActivity.java
+++ app/src/main/java/com/sombright/vizix/simultanea/MobCharacters/MobSelectionActivity.java
@@ -1,26 +1,32 @@
 package com.sombright.vizix.simultanea.MobCharacters;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.GridView;
 
 import com.sombright.vizix.simultanea.CharacterDetailsActivity;
 import com.sombright.vizix.simultanea.R;
 
 public class MobSelectionActivity extends AppCompatActivity {
 
+    private MediaPlayer mediaPlayer;
+    MobPool mAdapter;
+    private GridView gridview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mob_selection);
 
+
         // Instead of manually filling the grid with character icons, we connect
         // the GridView to an "adapter" whose job is to prepare each character
         // icon in a uniform way.
         gridview = findViewById(R.id.characterSelectionGridView);
-        mAdapter = new MobSelectionActivity(this, R.layout.character_selection_example_item, MobPool.charactersList);
+        mAdapter = new MobSelectionActivity(this, R.layout.activity_mob_selection,MobPool.mobList);
         gridview.setAdapter(mAdapter);
         gridview.setOnItemClickListener(this);
     }
