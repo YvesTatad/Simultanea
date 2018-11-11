6774e70496bee0bdcd23da0b10a93a2008dafac9
YvesTatad <41668249+YvesTatad@users.noreply.github.com>
2018-11-11 22:10:23 +0800

    Revert "check"
    This reverts commit 750b2fd1d7eac848b8d56208d84691cc88d1a943.

---
+++ app/src/main/java/com/sombright/vizix/simultanea/MobCharacters/MobDisplayActivity.java
@@ -0,0 +1,149 @@
+package com.sombright.vizix.simultanea.MobCharacters;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.sombright.vizix.simultanea.Player;
+import com.sombright.vizix.simultanea.R;
+
+
+public class MobDisplayActivity  extends ArrayAdapter<Player> implements View.OnClickListener {
+
+
+    private static final String TAG = "PlayersViewAdapter";
+    private final LayoutInflater mInflater;
+
+    private final Context mContext;
+    private final MobDisplayActivity.OnClickPlayerListener mListener;
+    private boolean mClickable = false;
+
+    static class ViewHolder {
+        TextView textView;
+        ImageButton imageButton;
+        ProgressBar progressBar;
+        int position;
+    }
+
+    MobDisplayActivity(@NonNull Context context, int resource, MobDisplayActivity.OnClickPlayerListener listener) {
+        super(context, resource);
+        mContext = context;
+        mInflater = LayoutInflater.from(context);
+        mListener = listener;
+    }
+
+    @Override
+    public @NonNull
+    View getView(int position, @Nullable View convertView,
+                 @NonNull ViewGroup parent) {
+       MobDisplayActivity.ViewHolder holder;
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.players_example_item, parent, false);
+            holder = new MobDisplayActivity.ViewHolder();
+            holder.imageButton = convertView.findViewById(R.id.imageButtonPlayer);
+            holder.imageButton.setScaleType(ImageView.ScaleType.FIT_START);
+            holder.imageButton.setAdjustViewBounds(true);
+            holder.imageButton.setOnClickListener(this);
+            holder.textView = convertView.findViewById(R.id.textViewPlayerName);
+            holder.progressBar = convertView.findViewById(R.id.progressBarPlayerHealth);
+            holder.progressBar.getProgressDrawable().setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+            holder.imageButton.setTag(holder);
+            holder.imageButton.setBackground(null);
+            convertView.setTag(holder);
+        } else holder = (ViewHolder) convertView.getTag();
+
+        final Player player = getItem(position);
+        boolean dead = player.getHealth() == 0;
+        if (dead) {
+            com.sombright.vizix.simultanea.Character character = player.getCharacter();
+            if (character == null) holder.imageButton.setImageResource(R.mipmap.ic_launcher);
+            else holder.imageButton.setImageResource(character.getDeadImageId());
+        } else {
+            AnimationDrawable animationDrawable = player.getAnimation();
+            if (animationDrawable == null)
+                holder.imageButton.setImageResource(R.mipmap.ic_launcher);
+            else {
+                holder.imageButton.setImageDrawable(animationDrawable);
+                animationDrawable.start();
+            }
+        }
+        holder.imageButton.setEnabled(mClickable && !dead);
+
+        holder.textView.setText(player.getName());
+        holder.progressBar.setProgress(player.getHealth());
+        holder.position = position;
+
+        return convertView;
+    }
+
+    public void setAnswered(boolean answered) {
+        for (int i = 0; i < getCount(); i++) {
+            final Player player = getItem(i);
+            if (player == null) continue;
+            player.setAnswered(false);
+        }
+    }
+
+    public boolean hasEveryoneAnswered() {
+        for (int i = 0; i < getCount(); i++) {
+            final Player player = getItem(i);
+            if (player == null) continue;
+            if (!player.hasAnswered()) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG, "onClick");
+        if (mListener != null) {
+            MobDisplayActivity.ViewHolder holder = (MobDisplayActivity.ViewHolder) v.getTag();
+            final Player player = getItem(holder.position);
+            mListener.onClickPlayer(player);
+        }
+    }
+
+    public interface OnClickPlayerListener {
+        void onClickPlayer(Player player);
+    }
+
+    public void setClickable(boolean clickable) {
+        Log.d(TAG, "setClickable " + clickable);
+        if (mClickable != clickable) {
+            mClickable = clickable;
+            // Rebuild the views
+            notifyDataSetChanged();
+        }
+    }
+
+    Player getPlayer(String uniqueId) {
+        for (int i = 0; i < getCount(); i++) {
+            final Player player = getItem(i);
+            if (player == null) continue;
+            if (player.getUniqueID().equals(uniqueId)) return player;
+        }
+        return null;
+    }
+
+
+
+    Player getPlayerByName(String name) {
+        for (int i = 0; i < getCount(); i++) {
+            final Player player = getItem(i);
+            if (player == null) continue;
+            if (player.getName().equals(name)) return player;
+        }
+        return null;
+    }
+}
